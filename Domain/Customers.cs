bd5d2574cb4b0483a4658e21f7bf8d2c04258922
Charmaine <cnmogotlane@gmail.com>
2024-11-14 11:55:00 +0200

    Setting Up the Project.

---
+++ Domain/Customers.cs
@@ -0,0 +1,23 @@
+namespace Domain
+{
+    public class Customers
+    {
+        public int CustomerID {get; set;}
+
+        public string IDNumber {get; set;}
+
+        public string FirstName {get; set;}
+
+        public string LastName {get; set;}
+
+        public DateOnly DateOfBirth {get; set;}
+
+        public string PhoneNumber {get; set;}
+
+        public string Email {get; set;}
+
+        public string Address {get; set;}
+
+        public DateTime RegistrationDate {get; set;}
+    }
+}
---
+++ Domain/LoanApplications.cs
@@ -0,0 +1,29 @@
+namespace Domain
+{
+    public class LoanApplications
+    {
+        public int ApplicationID {get; set;}
+
+        public int CustomerID {get; set;}
+
+        public int CreditScore {get; set;}
+
+        public int LoanTermMonths {get; set;}
+
+        public DateOnly ApplicationDate {get; set;}
+
+        public decimal LoanAmountRequested {get; set;}
+
+        public decimal InterestRate {get; set;}
+
+        public decimal CurrentExpenses {get; set;}
+
+        public decimal CurrentIncome {get; set;}
+
+        public string LoanType {get; set;}
+
+        public string Status {get; set;}
+
+        public string Remarks {get; set;} 
+    }
+}
